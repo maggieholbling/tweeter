3f8df919bb7c8b06c436fe285057d2e212fb05f3
Maggie <maggieholbl@gmail.com>
2019-09-11 22:07:57 +0000

    ajax call for submitting and loading tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,37 +1,12 @@
 $(document).ready(function() {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
   const createTweetElement = function(object) {
     const name = object.user.name;
     const avatars = object.user.avatars;
     const handle = object.user.handle;
     const text = object.content.text;
     const timestamp = Math.round((Date.now() - object.created_at) / (1000*60*60*24));
-    
+  
     const article = $(
     `<article class="tweet">
     <header>
@@ -59,13 +34,38 @@ $(document).ready(function() {
   </article>`);
     return article;
   }
-  
 
   const renderTweets = function(arrayOfObjects) {
+    $('container').text("");
     arrayOfObjects.forEach(obj => {
       $('container').append(createTweetElement(obj));
     });
   };
 
-  renderTweets(data);
+  const loadTweets = () => {
+    $.ajax('/tweets')
+    .then(renderTweets)
+    // .catch(err => {
+    //   alert('Error loading tweets')
+    // });
+  };
+
+  loadTweets();
+
+  //Hijacking form
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+    formData = $(this).serialize();
+    console.log(formData);
+
+    $.ajax('/tweets', {
+      method: 'POST',
+      data: formData
+    })
+    .then(loadTweets)
+    // .catch(err => {
+    //   alert('Error posting tweet')
+    // });
+  });
+
 });
